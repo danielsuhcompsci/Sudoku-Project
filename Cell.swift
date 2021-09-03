e97f0f92ecd14fb801ddc29a4a60b0ce076480c4
danielsuhcompsci <daniel.suh@student.allenisd.org>
2021-09-03 10:32:48 -0500

    Modified Cell.swift, added Enum.swift;

--- Cell.swift
+++ Cell.swift
@@ -1,6 +1,7 @@
 struct Cell{
     let row: Int
     let column: Int
+ 
     
     init(row:Int, column:Int){
         self.row = row
---
+++ Enum.swift
@@ -0,0 +1,11 @@
+enum boxType{
+    case center
+    case north
+    case northWest
+    case northEast
+    case south
+    case southWest
+    case southEast
+    case east
+    case west
+}
